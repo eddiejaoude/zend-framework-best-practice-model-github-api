989cfe2b5d55e71e4648866dbb82c2314992fd8d
Eddie Abou-Jaoude <eddieaboujaoude@Eddies-MacBook-Pro.local>
2013-02-10 13:30:41 +0000

    Removed duplicate services Exceptions directory

--- application/modules/github/models/services/Exceptions/Empty.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Class Github_Model_Service_Exception_Empty
- */
-class Github_Model_Service_Exception_Empty extends Github_Model_Service_Exception {
-
-}
--- application/modules/github/models/services/Exceptions/NoMapper.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Class Github_Model_Service_Exception_NoMapper
- */
-class Github_Model_Service_Exception_NoMapper extends Github_Model_Service_Exception {
-
-}
